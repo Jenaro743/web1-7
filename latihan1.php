78897e9f2acdbb5bc21ee39f363794a988205ee5
Mohamad Agip Jenaro <agipmohamad@gmail.com>
2025-11-20 19:18:10 +0700

    Add files via upload

---
+++ latihan1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Latihan 1</title>
+</head>
+<body>
+
+<h2>Bilangan Genap 1–10</h2>
+<?php
+for ($i = 1; $i <= 10; $i++) {
+    if ($i % 2 == 0) {
+        echo $i . " ";
+    }
+}
+?>
+
+</body>
+</html>
